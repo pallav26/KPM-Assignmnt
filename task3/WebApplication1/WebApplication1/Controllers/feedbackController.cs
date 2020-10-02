043cef5039e25b17ac7e61cd2de444bbf31d99c6
Pallav Nathany <pallav.nathany@ndsglobal.com>
2020-10-02 23:57:35 +0530

    Task 3 upload web app

---
+++ task3/WebApplication1/WebApplication1/Controllers/feedbackController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class feedbackController : Controller
+    {
+       
+        public ActionResult Create()
+        {
+            return View("Createfeedback");
+        }
+
+         
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Modelfeedback modelfeedback)
+        {
+            try
+            {
+                 
+
+                return RedirectToAction(nameof(thankYou));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult thankYou()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ task3/WebApplication1/WebApplication1/Models/Modelfeedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Modelfeedback
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Display (Name ="Email Address")]
+        [Required]
+        [EmailAddress (ErrorMessage ="The Email Address is Not valid")]
+        public string MailId { get; set; }
+        public string Country { get; set; }
+        [Required(ErrorMessage ="Feedback can not be blank")]
+        [DataType(DataType.MultilineText)]
+        public string Feedback { get; set; }
+
+    }
+}
