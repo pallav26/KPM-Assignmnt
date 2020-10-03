da5013690757704fddb6c8c37dbc2ce130957c44
pallav26 <pallavnathany1993@gmail.com>
2020-10-03 11:08:29 +0530

    Update feedbackController.cs

--- task3/WebApplication1/WebApplication1/Controllers/feedbackController.cs
+++ task3/WebApplication1/WebApplication1/Controllers/feedbackController.cs
@@ -1,10 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 
 namespace WebApplication1.Controllers
 {
@@ -23,7 +26,8 @@ namespace WebApplication1.Controllers
         {
             try
             {
-                 
+                string HashedComment = hashcode(modelfeedback.Feedback);
+                TempData.Add("CommentHashed", HashedComment);
 
                 return RedirectToAction(nameof(thankYou));
             }
@@ -35,7 +39,25 @@ namespace WebApplication1.Controllers
 
         public IActionResult thankYou()
         {
+
             return View();
+        }
+
+        public string hashcode(string comment )
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password : comment,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return hashed;
         }
 
        
